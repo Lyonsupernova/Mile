bec646a09c2f8dc3837333e444685377058a2468
Daoyi Lu <daoyilu@daoyis-air.lan>
2024-11-03 09:44:56 -0800

    add acr push containers scritps & helm skeleton folders

---
+++ deploy/DeployAcrImage.ps1
@@ -0,0 +1,33 @@
+
+# install az cli cmdhttps://docs.oracle.com/en/java/javase/11/docs/api/
+echo "Install az cli"
+brew install azure-cli
+
+# install docker
+echo "Install docker"
+brew install docker
+
+az login
+az account set -s 
+$subscriptionId = "2893e19b-1b1b-45ce-a107-5d171398d1d3"
+$resourceGroup = "Mile-rg-Global"
+$containerRegistry = "mileacrglobal"
+
+echo "login az acr"
+$tokenCredential = $(az acr login --name $containerRegistry -t | ConvertFrom-Json)
+
+# Login to ACR using access token
+docker login $tokenCredential.loginServer --username 00000000-0000-0000-0000-000000000000 --password $tokenCredential.accessToken
+
+# Tag your local Docker image
+docker build ./src -t mile-app
+
+docker tag mile-app:latest $containerRegistry/mile:latest
+
+# Push image to ACR
+echo "Push Mile image to ACR"
+docker push mileacrglobal.azurecr.io/mile:latest
+
+# list images in ACR
+echo "List images in ACR"
+az acr repository list --name $containerRegistry --output table
